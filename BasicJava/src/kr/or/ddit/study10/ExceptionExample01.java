8342791ba87ce13f366537c91b3c0f00cf649259
vasipoie <poieoe09@gmail.com>
2023-10-30 21:50:29 +0900

    ExceptionExample01에 try catch문 넣어보고 순서 바꿨을 때 출력이 되는지 확인

--- BasicJava/src/kr/or/ddit/study10/ExceptionExample01.java
+++ BasicJava/src/kr/or/ddit/study10/ExceptionExample01.java
@@ -3,15 +3,17 @@ package kr.or.ddit.study10;
 public class ExceptionExample01 {
 	public static void main(String[] args) {
 		ExceptionExample01 ee = new ExceptionExample01();
-		ee.devide(10, 3);
-		ee.devide(3, 0);
+		double de2 = ee.devide(10, 3);
+		System.out.println(de2);
+		double de1 = ee.devide(3, 0);
+		System.out.println(de1);
 	}
 	
 	public double devide(int a, int b) {
-		if(b==0) {
-			System.out.println("0으로 나눌 수 없습니다.");
-			return b;
-		}
+//		if(b==0) {
+//			System.out.println("0으로 나눌 수 없습니다.");
+//			return b;
+//		}
 		return a/b;
 	}
 }
--- BasicJava/src/kr/or/ddit/study10/ExceptionExample01re.java
+++ BasicJava/src/kr/or/ddit/study10/ExceptionExample01re.java
@@ -2,24 +2,34 @@ package kr.or.ddit.study10;
 
 public class ExceptionExample01re {
 	public static void main(String[] args) {
+		ExceptionExample01re ee = new ExceptionExample01re();
+		
 		try {
+			double de1 = ee.devide(3, 0);
+			System.out.println(de1);
+			double de2 = ee.devide(10, 3);
+			System.out.println(de2);
 			
+		} catch (ArithmeticException e) {
+			System.out.println("0으로 나눌 수 없습니다.");
+		}
+		try {
+			double de2 = ee.devide(10, 3);
+			System.out.println(de2);
+			double de1 = ee.devide(3, 0);
+			System.out.println(de1);
 		} catch (Exception e) {
-			// TODO: handle exception
+			System.out.println("0으로 나눌 수 없습니다.");
 		}
 		
 		
-		
-		ExceptionExample01re ee = new ExceptionExample01re();
-		ee.devide(10, 3);
-		ee.devide(3, 0);
 	}
 	
 	public double devide(int a, int b) {
-		if(b==0) {
-			System.out.println("0으로 나눌 수 없습니다.");
-			return b;
-		}
+//		if(b==0) {
+//			System.out.println("0으로 나눌 수 없습니다.");
+//			return b;
+//		}
 		return a/b;
 	}
 }
